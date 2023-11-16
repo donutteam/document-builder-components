62dd0072bf21054b5e433ce62ed9a665c5e01357
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-11-16 11:06:18 -0500

    Adding MapOrFallback utility component.

---
+++ src/components/utilities/MapOrFallback.ts
@@ -0,0 +1,19 @@
+//
+// Imports
+//
+
+import { Child } from "@donutteam/document-builder";
+
+//
+// Component
+//
+
+export function MapOrFallback<T>(items : T[], mapFunction : (item : T) => Child, fallback? : Child) : Child
+{
+	if (items.length === 0)
+	{
+		return fallback ?? null;
+	}
+
+	return items.map(mapFunction);
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+//
+// Exports
+//
+
+export * from "./components/utilities/MapOrFallback.js";
