806df7c1f5ecf1eca9b511b8ca533fc4083992b9
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-08-06 10:29:43 -0400

    Improving Notice client code.

--- src/components/Notice.client.ts
+++ src/components/Notice.client.ts
@@ -1,17 +1,50 @@
 //
+// Imports
+//
+
+import { Notice, NoticeOptions } from "./Notice.js";
+
+//
+// Local Functions
+//
+
+function initialiseNotice(notice: HTMLElement)
+{
+	notice.classList.add("initialised");
+
+	const dismissElement = notice.querySelector(".dismiss");
+
+	if (dismissElement == null)
+	{
+		return;
+	}
+
+	dismissElement.addEventListener("click",
+		() =>
+		{
+			notice.remove();
+		});
+}
+
+//
 // Component
 //
 
+export function createNotice(options: NoticeOptions): HTMLElement
+{
+	const notice = Notice(options).renderToHTMLElement();
+
+	initialiseNotice(notice);
+
+	return notice;
+}
+
 export function initialiseNotices()
 {
-	const notices = document.querySelectorAll(".component-notice");
+	const notices = document.querySelectorAll(".component-notice:not(.initialised)") as NodeListOf<HTMLElement>;
 
 	for (const notice of notices)
 	{
-		notice.querySelector(".dismiss")?.addEventListener("click",
-			() =>
-			{
-				notice.remove();
-			});
+		initialiseNotice(notice);
 	}
 }
